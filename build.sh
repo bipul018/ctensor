781bee9e661688e1b3a38dfad0583b96227317b8
Bipul Neupane <83596423+bipul018@users.noreply.github.com>
2025-04-05 15:15:53 +0545

    added compiler detection improved error message in build.sh

--- build.sh
+++ build.sh
@@ -14,16 +14,26 @@ fi
 
 # Check if the directory exists
 if [[ ! -d "$UTILS_PATH" ]]; then
-    echo "Utilities directory not found."
+    echo "Utilities directory not found at "$UTILS_PATH
     echo "Clone from https://github.com/bipul018/c-utils"
     echo "  and paste it in the parent directory"
     echo "  or set 'UTILS_PATH' environment variable correctly."
     exit 1  # Exit with an error code
 fi
 
-echo "Compiling core object files..."
-cc -c -I$UTILS_PATH tensor.c -o ./build/tensor.obj
+# Find the compiler
+if command -v gcc &> /dev/null; then
+    CC='gcc'
+elif command -v clang &> /dev/null; then
+    CC='clang'
+else
+    echo "Neither gcc nor clang is installed. Please install one of them."
+    exit 1
+fi
+
+echo "Compiling core object files using $CC ..."
+$CC -c -I$UTILS_PATH tensor.c -o ./build/tensor.obj
 echo "Compiling examples file..."
-cc -I$UTILS_PATH ./build/tensor.obj examples.c -o ./build/examples
+$CC -I$UTILS_PATH ./build/tensor.obj examples.c -o ./build/examples
 echo "Compiled!"
    
