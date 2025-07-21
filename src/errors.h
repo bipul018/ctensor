e44acece0dfbf1dcb77bb8935b35fd3617dcd5eb
Bipul Neupane <83596423+bipul018@users.noreply.github.com>
2025-07-21 06:32:53 +0545

    new sane code with test and compilation begins

--- src/errors.h
+++ src/errors.h
@@ -1,53 +1,99 @@
 #ifndef ERRORS_H
 #define ERRORS_H
 
+#ifdef ERRORS_H_SOLO_RUN_TEST
+#define ERRORS_H_IMPL
+#endif //ERRORS_H_SOLO_RUN_TEST
 
 #define UTIL_INCLUDE_ALL
 #include <util_headers.h>
 
 typedef struct Error_Chain Error_Chain;
 struct Error_Chain{
-  // A null terminated error message allocated from a global pool somewhere (preferably)
-  const char* err_msg;
   // nullptr => end of the error chain,
   // -1 => a free error node (used for creating error pool)
   Error_Chain* prev;
+  // A null terminated error message allocated just with this struct
+  char msg[1];
 };
-DEF_SLICE(Error_Chain);
-struct Global_Error_Chain_Pool;
-extern const struct Global_Error_Chain_Pool* global_error_pool;
-
 
 // When making a new error chain node, prefer to rather erase the head and use it as tail
 //   rather than returning error (It probably is better to report the latest error in the chain)
 // Only if this is a fresh error, then you can return nullptr (or you probably can also assert that part)
 void init_global_error_chain_pool(Alloc_Interface allocr, size_t count);
 void free_global_error_chain_pool(Alloc_Interface allocr);
-Error_Chain* new_error(Error_Chain* prev_err, const char* file_func, int lineno, const char* fmt, ...);
+Error_Chain* new_error(Error_Chain* prev_err, const char* file, const char* func, int lineno, const char* fmt, ...);
 // TODO:: Find out if this is even worth doing at all
-void free_error_chain(Error_Chain* err_chain);
+void free_error(Error_Chain* err_chain);
+
 
 #ifdef ERRORS_H_IMPL
+#include<stdarg.h>
+#include<stdio.h>
+#include<stddef.h>
 
-DEF_SLICE(size_t);
-typedef struct Error_Buffer_Unit Error_Buffer_Unit;
-struct Error_Buffer_Unit{
-  Error_Buffer_Unit* next;
-  size_t size;
+// The union that actually represents the underlying storage of error chains
+typedef union Error_Pool_Unit Error_Pool_Unit;
+union Error_Pool_Unit {
+  Error_Chain chn;
+  struct{
+    Error_Pool_Unit* next;
+    size_t sz;
+  };
 };
-DEF_SLICE(Error_Buffer_Unit);
+DEF_SLICE(Error_Pool_Unit);
 static struct {
-  Error_Chain_Slice pool;
-  size_t curr;
-  // Mostly only used during init and freeing
-  Error_Buffer_Unit_Slice msg_pool;
-  Error_Buffer_Unit* free_msgs;
-} global_error_pool_impl = {0};
-const struct Global_Error_Chain_Pool* global_error_pool = &global_error_pool_impl;
+  Error_Pool_Unit_Slice pool;
+  Error_Pool_Unit* free_units;
+} global_error_pool = {0};
+
+void init_global_error_chain_pool(Alloc_Interface allocr, size_t count){
+  global_error_pool.pool = SLICE_ALLOC(allocr, Error_Pool_Unit, count);
+  assert(global_error_pool.pool.data != nullptr);
+  assert(global_error_pool.pool.count != 0);
+
+  global_error_pool.free_units = global_error_pool.pool.data;
+  global_error_pool.free_units->next = nullptr;
+  global_error_pool.free_units->sz =  global_error_pool.pool.count;
+}
+void free_global_error_chain_pool(Alloc_Interface allocr){
+  SLICE_FREE(allocr, global_error_pool.pool);
+  global_error_pool.free_units = nullptr;
+}
+
+// function to write assuming its enough, returns leftover (if it was left)
+static Error_Pool_Unit* write_err_v(Error_Pool_Unit* err, const char* file, const char* func, int lineno, const char* fmt, va_list args){
+  Error_Pool_Unit nerr = {0};
+  if(err != nullptr){
+    nerr = *err;
+  }
+  const size_t max_sz = ((err == nullptr)?0:(sizeof(Error_Pool_Unit) * err->sz - offsetof(Error_Chain, msg)));
+  char* dst = ((err==nullptr)?nullptr:err->chn.msg);
+  const size_t sz1 = snprintf(dst, max_sz, "%s:%d:(%s): ",
+		  (file == nullptr?"":file),lineno,(func == nullptr?"":func));
+  const size_t sz2 = vsnprintf(dst + ((max_sz == 0)?0:sz1),
+			       max_sz - ((max_sz == 0)?0:sz1),
+			       fmt, args);  
+  const size_t consumed = sz1 + sz2 + 1 + offsetof(Error_Chain, msg);
+
+  const size_t unit_cnt = (consumed + sizeof(Error_Pool_Unit) - 1) / sizeof(Error_Pool_Unit);
+  if(err != nullptr && unit_cnt < nerr.sz){
+    err[unit_cnt] = nerr;
+    err[unit_cnt].sz -= unit_cnt;
+    return err + unit_cnt;
+  }
+  return nullptr;
+}
+
+// function to find out the no of nodes consumed
+static size_t calc_no_units_v(const char* file, const char* func, int lineno, const char* fmt, va_list args){
+  return (size_t)(write_err_v(nullptr, file, func, lineno, fmt, args) - (Error_Pool_Unit*)nullptr);
+}
 
 // Given an msg pool, get the >= matching if exists or the end (also removes it)
 // Or returns the last entry
-Error_Buffer_Unit* get_appropriate_bfr(Error_Buffer_Unit** p_free_node, size_t msg_len){
+/*
+Error_Pool_Unit* get_appropriate_bfr(Error_Pool_Unit** p_free_node, size_t units){
   assert(p_free_node != nullptr && (*p_free_node) != nullptr);
 
   while((*p_free_node)->next != nullptr &&
@@ -82,12 +128,43 @@ void free_error(Error_Chain* root){
 
   
 }
-
+*/
+void free_error(Error_Chain* err_chain){
+  assert(("Not implemented yet!!!", false));
+}
 Error_Chain* new_error(Error_Chain* prev_err, const char* file, const char* func, int lineno, const char* fmt, ...){
   // First find out if any error is free
+  if(global_error_pool.free_units == nullptr){
+    // Try to reclaim from the top of prev_err
+    // TODO:: Figure out a way for this, or a better assert message
+    assert(prev_err != nullptr);
+    Error_Chain** ptop = &prev_err;
+    while((*ptop)->prev != nullptr) ptop = &(*ptop)->prev;
+    Error_Chain* top = *ptop;
+    *ptop = nullptr;
+    // Reinsert this into the free_units
+    free_error(top);
+    // Now at least one should be available as the free error
+  }
+  // Now calculate the required size
+  va_list args = {0};
+
+  va_start(args, fmt);
+  size_t node_num = calc_no_units_v(file, func, lineno, fmt, args);
+  va_end(args);
+
+  // Find one from the free list (which should have at least one)
   
-  Error_Chain* perr = nullptr;
-  for_slice(global_error_pool_impl.pool, i){
+  // Write on the error
+
+  // If there was more, reinsert the remaining part
+
+  // Return the error node
+
+  
+  /*
+  Error_Pool_Unit* perr = nullptr;
+  for_slice(global_error_pool.pool, i){
     Error_Chain* maybe = &slice_inx(global_error_pool_impl.pool,
 				    (i+global_error_pool_impl.curr)%
 				    global_error_pool_impl.pool.count);
@@ -158,7 +235,83 @@ Error_Chain* new_error(Error_Chain* prev_err, const char* file, const char* func
   perr->prev = prev_err;
   perr->err_msg = (const char*)ch_buf;
   return perr;
+  */
+  return nullptr;
 }
 
 #endif // ERRORS_H_IMPL
+
+
+#ifdef ERRORS_H_SOLO_RUN_TEST
+// gcc -DERRORS_H_KEEP_EXE -I$HOME/prgming/c-utils/ -xc errors.h -DERRORS_H_SOLO_RUN_TEST -o errors.out && ./errors.out
+#include <stdio.h>
+
+static size_t calc_no_units(const char* file, const char* func, int lineno, const char* fmt, ...){
+  va_list args = {0};
+  va_start(args, fmt);
+  size_t ans = calc_no_units_v(file, func, lineno, fmt, args);
+  va_end(args);
+  return ans;
+}
+static Error_Pool_Unit* write_err(Error_Pool_Unit* err, const char* file, const char* func, int lineno, const char* fmt, ...){
+  va_list args = {0};
+  va_start(args, fmt);
+  Error_Pool_Unit* ans = write_err_v(err, file, func, lineno, fmt, args);
+  va_end(args);
+  return ans;
+}
+
+int main(int argc, char* argv[]){
+
+#ifndef ERRORS_H_KEEP_EXE
+  // First delete self
+  assert(argc >= 1);
+  printf("Deleting self at %s...\n", argv[0]);
+  remove(argv[0]);
+#endif //ERRORS_H_KEEP_EXE
+
+  Alloc_Interface allocr = gen_std_allocator();
+  init_global_error_chain_pool(allocr, 2048);
+
+  printf("%p, %zu, %p, %p, %zu!\n",
+	 global_error_pool.pool.data,
+	 global_error_pool.pool.count,
+	 global_error_pool.free_units,
+	 global_error_pool.free_units->next,
+	 global_error_pool.free_units->sz);
+  
+  size_t sz = calc_no_units(__FILE__, __func__, __LINE__, "%s", "hi");
+
+  printf("Sizeof error struct : %zu\n", sizeof(Error_Pool_Unit));
+  printf("The required size is: %zu\n", sz);
+
+  Error_Pool_Unit errs[2] = {0};
+  errs->sz = _countof(errs);
+  errs->next = (void*)0x1234; // Just a dummy value
+
+  Error_Pool_Unit* nerr = write_err(errs, __FILE__, __func__, __LINE__, "%s", "hi");
+
+  printf("The error is \n*************\n%s\n***********\n", errs->chn.msg);
+  
+  printf("The current error is %p, next error is %p, difference is %zu\n",
+	 errs, nerr, (nerr-errs));
+  printf("The usedup length is %zu\n",
+	 1 + strlen(errs->chn.msg) + offsetof(Error_Chain, msg));
+  if(nerr != nullptr) printf("The value in next error is: %p, %zu\n",
+			     nerr->next, nerr->sz);
+
+
+
+  Error_Chain* err = new_error(nullptr, __FILE__, __func__, __LINE__, "Couldnt do jack shit");
+
+  
+  free_global_error_chain_pool(allocr);
+  return 0;
+}
+
+
+#endif //ERRORS_H_SOLO_RUN_TEST
+
+
+
 #endif // ERRORS_H
